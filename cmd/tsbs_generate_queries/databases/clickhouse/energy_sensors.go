c1c5e4cc69ab3b1032afe762acbc26e468103c81
Jannik Möll <jannik.moell+MoellJ@gmail.com>
2023-08-24 17:37:07 +0200

    Finished ClickHouse queries

--- cmd/tsbs_generate_queries/databases/clickhouse/energy_sensors.go
+++ cmd/tsbs_generate_queries/databases/clickhouse/energy_sensors.go
@@ -38,18 +38,23 @@ func (d *EnergySensors) getSensorsWhereString(nSensors int) string {
 
 func (d *EnergySensors) LastPointForSensors(qi query.Query, nSensors int) {
 	var sql string
-
-	sql = fmt.Sprintf(`SELECT *
-		FROM 
+	if d.UseTags {
+		sql = fmt.Sprintf(`SELECT * FROM 
 		(
-			SELECT *
-			FROM readings
-			WHERE (tags_id, time) IN 
+			SELECT * FROM readings WHERE (tags_id, time) IN 
 			(
 				SELECT tags_id, max(time) FROM readings GROUP BY tags_id
 			)
 		) AS r INNER JOIN tags AS t ON r.tags_id = t.id WHERE %s`,
-		d.getSensorsWhereString(nSensors))
+			d.getSensorsWhereString(nSensors))
+	} else {
+
+		sql = fmt.Sprintf(`SELECT * FROM readings WHERE %s AND (sensorname, created_at) IN
+			  (
+				  SELECT sensorname, max(created_at) FROM readings GROUP BY sensorname
+			  )`,
+			d.getSensorsWhereString(nSensors))
+	}
 	humanLabel := "ClickHouse last row for sensors"
 	humanDesc := humanLabel
 	d.fillInQuery(qi, humanLabel, humanDesc, "readings", sql)
@@ -58,38 +63,54 @@ func (d *EnergySensors) LastPointForSensors(qi query.Query, nSensors int) {
 func (d *EnergySensors) HistoryForSensors(qi query.Query, nSensors int, timeRange time.Duration) {
 	interval := d.Interval.MustRandWindow(timeRange)
 	var sql string
-
-	sql = fmt.Sprintf(`SELECT *
+	if d.UseTags {
+		sql = fmt.Sprintf(`SELECT *
 		FROM 
 		(
-			SELECT *
-			FROM readings
+			SELECT * FROM readings 
 			WHERE (created_at >= toDateTime('%s')) AND (created_at < toDateTime('%s'))
 		) AS r INNER JOIN tags AS t ON r.tags_id = t.id WHERE %s`,
-		interval.Start().Format("2006-01-02 15:04:05"),
-		interval.End().Format("2006-01-02 15:04:05"),
-		d.getSensorsWhereString(nSensors))
+			interval.Start().Format(clickhouseTimeStringFormat),
+			interval.End().Format(clickhouseTimeStringFormat),
+			d.getSensorsWhereString(nSensors))
+	} else {
+		sql = fmt.Sprintf(`SELECT * FROM readings
+			WHERE %s AND (created_at >= toDateTime('%s')) AND (created_at < toDateTime('%s'))`,
+			d.getSensorsWhereString(nSensors),
+			interval.Start().Format(clickhouseTimeStringFormat),
+			interval.End().Format(clickhouseTimeStringFormat))
+	}
 	humanLabel := "ClickHouse history for sensors"
 	humanDesc := humanLabel
 	d.fillInQuery(qi, humanLabel, humanDesc, "readings", sql)
 }
 
-//TODO
 func (d *EnergySensors) AggregateForSensors(qi query.Query, nSensors int, timeRange time.Duration, aggInterval time.Duration, aggregate string) {
 	interval := d.Interval.MustRandWindow(timeRange)
 	if aggregate == energy_sensors.AggRand {
 		aggregate = common.RandomStringSliceChoice(energy_sensors.AggChoices)
 	}
-	aggClause := fmt.Sprintf("%[1]s(value) as %[1]s_value", aggregate)
+	if aggregate == energy_sensors.AggStdDev {
+		aggregate = "stddevPop"
+	}
+	if aggregate == energy_sensors.AggStdDev {
+		aggregate = "varPop"
+	}
+	aggClause := fmt.Sprintf("%[1]s(value)", aggregate)
 	var sql string
-	sql = fmt.Sprintf(`SELECT time_bucket('%s seconds', time) as timeframe, sensorname, %s FROM readings 
-                            WHERE %s and time >= '%s' and time < '%s'
-                            GROUP BY timeframe, sensorname ORDER BY by by by timeframe`,
-		fmt.Sprintf("%f", aggInterval.Seconds()),
-		aggClause,
-		d.getSensorsWhereString(nSensors),
-		interval.StartString(),
-		interval.EndString())
+
+	if d.UseTags {
+		panic("not implemented") //TODO
+	} else {
+		sql = fmt.Sprintf(`SELECT sensorname, toStartOfInterval(created_at, toIntervalSecond(%d)) AS timeinterval, %s
+			FROM readings
+			WHERE %s AND (created_at >= toDateTime('%s')) AND (created_at < toDateTime('%s')) GROUP BY timeinterval, sensorname`,
+			int(aggInterval.Seconds()),
+			aggClause,
+			d.getSensorsWhereString(nSensors),
+			interval.Start().Format(clickhouseTimeStringFormat),
+			interval.End().Format(clickhouseTimeStringFormat))
+	}
 
 	humanLabel := "TimescaleDB aggregated history for sensors"
 	humanDesc := humanLabel
